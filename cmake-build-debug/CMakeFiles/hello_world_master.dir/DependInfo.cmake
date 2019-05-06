c039dee2d131912e446d664f95de0c06120a0544
drmoody <drmoody@dactm.com>
2019-05-05 21:20:46 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/hello_world_master.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dan/CLionProjects/hello-world-master/hello.cpp" "/home/dan/CLionProjects/hello-world-master/cmake-build-debug/CMakeFiles/hello_world_master.dir/hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/hello_world_master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_master.dir/hello.cpp.o"
+  "hello_world_master.pdb"
+  "hello_world_master"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
